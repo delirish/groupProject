d7e3a2445415a1f12cb683556da4909f1e027772
delirish <delitrion5694>
2019-11-09 14:42:27 -0600

    branch practice

--- src/firstClass.java
+++ src/firstClass.java
@@ -1,5 +1,9 @@
 public class firstClass {
     public static void main(String[] args) {
         System.out.println("First line");
+
+        if(true){
+            System.out.println("Second line");
+        }
     }
 }
